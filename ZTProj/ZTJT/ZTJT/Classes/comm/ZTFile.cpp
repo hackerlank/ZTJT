e068e4a341ffa8149fa21ae70552b8455e578b01
perry0002 <perryxiong@163.com>
2014-03-21 18:20:21 +0800

    增加ZTFile

---
+++ ZTProj/ZTJT/ZTJT/Classes/comm/ZTFile.cpp
@@ -0,0 +1,19 @@
+//
+//  ZTFile.cpp
+//  ZTJT
+//
+//  Created by PerryMac on 3/21/14.
+//  Copyright (c) 2014 PerryMac. All rights reserved.
+//
+
+#include "ZTFile.h"
+
+ZTFile::ZTFile()
+{
+    
+}
+
+ZTFile::~ZTFile()
+{
+    
+}
---
+++ ZTProj/ZTJT/ZTJT/Classes/comm/ZTFile.h
@@ -0,0 +1,18 @@
+//
+//  ZTFile.h
+//  ZTJT
+//
+//  Created by PerryMac on 3/21/14.
+//  Copyright (c) 2014 PerryMac. All rights reserved.
+//
+
+#ifndef __ZTJT__ZTFile__
+#define __ZTJT__ZTFile__
+
+class ZTFile {
+public:
+    ZTFile();
+    ~ZTFile();
+};
+
+#endif /* defined(__ZTJT__ZTFile__) */
